32a0b4e910b5fd1c14206ffc37ec4d2829b9c784
Ptolemy <ptolemybarnes@gmail.com>
2015-06-23 18:06:10 +0100

    Scaffolded city temperature reporter

---
+++ lib/city_temperature_reporter.rb
@@ -0,0 +1,4 @@
+class CityTemperatureReporter
+
+
+end
---
+++ spec/city_temperature_reporter_spec.rb
@@ -0,0 +1,13 @@
+require './lib/city_temperature_reporter'
+
+describe CityTemperatureReporter do
+
+  it 'requests its current temperature' do
+    # think about what CityTemperatureReporter needs to do its job
+    # How can you test that it makes this request? Use diagrams to help you visualise this
+  end
+
+  it 'reports its current temperature' do
+    # once it's got that temperature, can it report it?
+  end
+end
